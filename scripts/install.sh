6b19cb1e6581fd1d11d15a8413ca7c93eecfd443
Petra Cihalova <pcihalov@redhat.com>
2021-09-15 13:25:35 +0200

    Added if-else conditions when repo is missing

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --login
 
 cwd=$(pwd)
+
 if [[ $cwd != *"topological_inventory-guides/scripts" ]]; then
   echo "Please switch to 'topological_inventory-guides/scripts' directory"
   exit
@@ -27,20 +28,20 @@ cd ${root_dir}
 #
 if [[ ! -d './kafka' ]]; then
     echo "-------------------------------------------------------"
-#    echo "Downloading Kafka..."
-#    wget -O kafka.tgz $KAFKA_INSTALL_URL
-#    tar -xzf kafka.tgz
-#    mv ./kafka_* ./kafka
+    echo "Downloading Kafka..."
+    wget -O kafka.tgz $KAFKA_INSTALL_URL
+    tar -xzf kafka.tgz
+    mv ./kafka_* ./kafka
 fi
 
 cd ${cwd}
 #
 # 3) make all scripts executable, create /scripts symlink
 #
-#echo "-------------------------------------------------------"
-#echo "Making these scripts executable, creating symlink..."
-#chmod 744 ./*.sh
-#chmod 744 ./services/*.sh
+echo "-------------------------------------------------------"
+echo "Making these scripts executable, creating symlink..."
+chmod 744 ./*.sh
+chmod 744 ./services/*.sh
 
 [ -L ${root_dir}/scripts ] || ln -s ${cwd} ${root_dir}/scripts
 
@@ -61,29 +62,32 @@ echo "Bundle all repos"
 gem install bundler
 ./bundle.sh update
 
-# 
+#
 # 6) create database.yml based on topological_inventory-api's/sources-api's database.dev.yml
-# 
+#
 echo "-------------------------------------------------------"
 echo "Copying database.yml from topological_inventory-api/config/database.dev.yml..."
 
-#cd ${TOPOLOGICAL_API_DIR}
-#dev_ymls=("config/database.yml"
-       #   "../topological_inventory-core/config/database.yml"
-       #   "../topological_inventory-persister/config/database.yml"
-       #   "../topological_inventory-ingress_api/config/database.yml"
-       #   "../topological_inventory-sync/config/database.yml"
- #          "../sources-api")
+if [ -d ${TOPOLOGICAL_API_DIR} ]; then
+    cd ${TOPOLOGICAL_API_DIR}
+    dev_ymls=("config/database.yml"
+              "../topological_inventory-core/config/database.yml"
+              "../topological_inventory-persister/config/database.yml"
+              "../topological_inventory-ingress_api/config/database.yml"
+              "../topological_inventory-sync/config/database.yml")
 
-#echo "Copying ${TOPOLOGICAL_API_DIR}/config/database.dev.yml"
-#for dest_path in ${dev_ymls[@]}; do
-#    if [[ -f ${dest_path} ]]; then
-#        echo "[SKIPPED] File already exists: ${dest_path}"
-#    else
-#        cp config/database.dev.yml ${dest_path}
-#        echo "[OK] Copied to: ${dest_path}"
-#    fi
-#done
+    echo "Copying ${TOPOLOGICAL_API_DIR}/config/database.dev.yml"
+    for dest_path in ${dev_ymls[@]}; do
+        if [[ -f ${dest_path} ]]; then
+            echo "[SKIPPED] File already exists: ${dest_path}"
+        else
+            cp config/database.dev.yml ${dest_path}
+            echo "[OK] Copied to: ${dest_path}"
+        fi
+    done
+else
+    echo "Info: Directory ${TOPOLOGICAL_API_DIR} does not exists. Skipping this step."
+fi
 
 echo "-------------------------------------------------------"
 echo "Copying ${SOURCES_API_DIR}/config/database.dev.yml"
@@ -118,30 +122,36 @@ do
     fi
 done
 
-#
 # 8) Configure UI
-#
-#echo "-------------------------------------------------------"
-#cd "$root_dir/insights-proxy"
-#npm install
+echo "-------------------------------------------------------"
+if [ -d ${INSIGHTS_PROXY_DIR} ]; then
+    cd ${INSIGHTS_PROXY_DIR}
+    npm install
 
-#echo "Patching /etc/hosts to connect localhost with CI/QA servers, please provide sudo credentials"
-#sudo bash scripts/patch-etc-hosts.sh
-#bash scripts/update.sh
+    echo "Patching /etc/hosts to connect localhost with CI/QA servers, please provide sudo credentials"
+    sudo bash scripts/patch-etc-hosts.sh
+    bash scripts/update.sh
+else
+    echo "Info: Directory ${INSIGHTS_PROXY_DIR} does not exists. Skipping this step."
+fi
 
-#cd "$root_dir/sources-ui"
-#npm install
+if [ -d ${SOURCES_UI_DIR} ]; then
+    cd ${SOURCES_UI_DIR}
+    npm install
+else
+    echo "Info: Directory ${SOURCES_UI_DIR} does not exists. Skipping this step."
+fi
 
-#echo "-------------------------------------------------------"
-#echo "Successfully installed!"
-#echo ""
-#echo "--- And what next? ---"
-#echo "* Check all database.yml files (see above) and change db name if needed"
-#echo "* Run ./db/init.sh to create and initialize database, or:"
-#echo "    run ./db/reset.sh to reset and initialize database if exists"
-#echo "* Run ./start.sh to run persister and api services in TMUX"
-#echo "* Run ./start.sh <svc> to run service (run ./list-services.sh to see list of services)"
-#echo ""
+echo "-------------------------------------------------------"
+echo "Successfully installed!"
+echo ""
+echo "--- And what next? ---"
+echo "* Check all database.yml files (see above) and change db name if needed"
+echo "* Run ./db/init.sh to create and initialize database, or:"
+echo "    run ./db/reset.sh to reset and initialize database if exists"
+echo "* Run ./start.sh to run persister and api services in TMUX"
+echo "* Run ./start.sh <svc> to run service (run ./list-services.sh to see list of services)"
+echo ""
 
 LANG=C DOW=$(date +"%a")
 echo "Happy $DOW! :)"
